6196757cb19b18d236a8043c6de565e9e46ecdf6
ayusharora <aarora7@dons.usfca.edu>
2019-10-15 18:50:32 -0700

    sql - sql queries updated

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -60,6 +60,7 @@ CREATE TABLE `customer` (
   `company_name` varchar(99) DEFAULT NULL,
   `company_website` varchar(145) DEFAULT NULL,
   `type_id` int(11) DEFAULT NULL,
+  `tin_no` varchar(9) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uq_email_user_id` (`email`,`user_id`),
   KEY `fk_user_idx` (`user_id`),
@@ -84,3 +85,50 @@ CREATE TABLE `invoices` (
   CONSTRAINT `fk_usr` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `created_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `sku` varchar(99) NOT NULL COMMENT 'Stock Keeping Unit Number',
+  `label` varchar(145) DEFAULT NULL,
+  `rate` decimal(6,2) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_sku_user_id` (`sku`),
+  KEY `idx_sku` (`sku`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mobile` bigint(20) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(125) DEFAULT NULL,
+  `name` varchar(127) DEFAULT NULL,
+  `type_id` int(11) NOT NULL,
+  `company_name` varchar(45) DEFAULT NULL,
+  `company_website` varchar(245) DEFAULT NULL,
+  `auth_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mobile_UNIQUE` (`mobile`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `ix_user_created_on` (`created_on`),
+  KEY `ix_user_id` (`id`),
+  KEY `fk_type_id_idx` (`type_id`),
+  CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `user_type` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `ix_user_type_created_on` (`created_on`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
